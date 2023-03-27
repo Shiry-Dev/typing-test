d9cef3f50b10000b700625a60a629256c5b2d81c
Gabriel Arrien <garrien1@gmail.com>
2023-03-27 19:13:42 -0300

    Setting test time to 60 seconds and altering (Ctrl + d) to ending test

--- src/typingSpeedTest.ts
+++ src/typingSpeedTest.ts
@@ -22,22 +22,39 @@ const fetchText = async (url: string): Promise<string> => {
   const sentence = await fetchText(gitHubURL);
 
   console.log('Welcome to the typing speed test!');
-  console.log(`Type the following sentence as quickly and accurately as you can:`);
+  console.log(`Type the following sentence as quickly and accurately as you can - You hava 60 seconds:`);
   console.log('\n' + sentence + '\n');
 
   const startTime = Date.now();
+  let userInput = '';
 
-  rl.question('Start typing: ', (userInput: string) => {
-    const endTime = Date.now();
-    const timeTaken = (endTime - startTime) / 1000;
+  const timer = setTimeout(() => {
+    console.log('\nTime\'s up! Stopping the test.');
+    process.exit(0);
+  }, 60000);
 
-    const wordsPerMinute = (userInput.length / 5) / (timeTaken / 60);
-    const accuracy = (100 * userInput.split('').filter((char, index) => char === sentence[index]).length) / sentence.length;
+  process.stdin.on('keypress', (str, key) => {
+	  if(key.ctrl && key.name === 'd') {
+		  clearTimeout(timer);
+		  const endTime = Date.now();
+		  const timeTaken = (endTime - startTime) / 1000;
 
-    console.log(`\nTime taken: ${timeTaken.toFixed(2)} seconds`);
-    console.log(`Words per minute: ${wordsPerMinute.toFixed(2)}`);
-    console.log(`Accuracy: ${accuracy.toFixed(2)}%`);
+		  const wordsPerMinute = (userInput.length / 5) / (timeTaken / 60);
+		  const accuracy = (100 * userInput.split('').filter((char, index) => char === sentence[index]).length) / sentence.length;
 
-    rl.close();
+	      console.log(`\nTime taken: ${timeTaken.toFixed(2)} seconds`);
+		  console.log(`Words per minute: ${wordsPerMinute.toFixed(2)}`);
+		  console.log(`Accuracy: ${accuracy.toFixed(2)}%`);
+
+		  rl.close();
+		  process.stdin.removeAllListeners('keypress');
+    } else {
+      userInput += str;
+    }
   });
+
+  console.log('Start typing (press (Ctrl + d)  to stop): ');
+  process.stdin.setRawMode(true);
+		
+  
 })();
